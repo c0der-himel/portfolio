076221e73ad5a00463d9eab90977342b8d7c794f
himel <himel777hussain@gmail.com>
2021-03-12 16:12:07 +0600

    updated

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,22 +1,15 @@
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 const Success = () => {
-    return (
-        <section
-            className="text-gray-400 bg-gray-900 body-font relative z-0"
-            id="contact"
-        >
-            <div className="container px-5 mx-auto">
-                <div className="lg:w-4/6 mx-auto">
-                    <div className="p-2 mb-10 w-full text-center">
-                        <p className="text-green-400">
-                            Message successfully set to
-                            himel777hussain@gmail.com
-                        </p>
-                    </div>
-                    <div className="p-2 w-full border-t border-gray-800 text-center"></div>
-                </div>
-            </div>
-        </section>
-    );
+    toast.configure();
+
+    useEffect(() => {
+        toast.success('Message sent successfully');
+    }, []);
+
+    return <div></div>;
 };
 
 export default Success;
